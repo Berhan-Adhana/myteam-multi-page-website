dddf88e18b00192726aca2215c7a2a70e7f62c73
berhan-helloworld <simonadhana@gmail.com>
2023-03-04 19:18:33 -0700

    variable name renamed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -13,25 +13,25 @@ const Button = ({
   //  1.Light
   //  2.Dark
   // Secondary
-  let clas;
+  let dynamicClass;
   if (primaryLight) {
-    clas =
+    dynamicClass =
       "bg-transparent border-2 border-white hover:bg-white text-white hover:text-[var(--dark-green)] disabled:opacity-[0.25] disabled:mix-blend-normal";
   }
   if (primaryDark) {
-    clas =
+    dynamicClass =
       "border-2 border-[var(--sacramento-state-green)] text-[var(--sacramento-state-green)] hover:bg-[var(--sacramento-state-green)] hover:text-white disabled:border-white disabled:opacity-[.25] disabled:mix-blend-normal";
   }
   if (secondary) {
-    clas =
+    dynamicClass =
       "bg-white text-[var(--dark-green)] border-2  hover:bg-[var(--rapture-blue)] hover:text-[var(--dark-green)] hover:border-[var(--rapture-blue)] disabled:opacity-[.25] disabled:mix-blend-normal";
   }
 
   return (
     <button
-      disabled={disabled ? true : false}
+      disabled={disabled}
       onClick={onClick}
-      className={` ${clas}  border-solid  cursor-pointer font-bold  border-[2px] py-3 px-8 rounded-full whitespace-nowrap `}
+      className={` ${dynamicClass}  border-solid  cursor-pointer font-bold  border-[2px] py-3 px-8 rounded-full whitespace-nowrap `}
     >
       {children}
     </button>
